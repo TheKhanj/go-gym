3b0199ca5ad019b9c2e8b9781ce1829cd913e9b2
Pooyan Khanjankhani <pooyankhan@gmail.com>
2023-07-31 15:22:21 +0330

    init only! nothing here...

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/thekhanj/go-gym
+
+go 1.20
+
+require github.com/thoas/go-funk v0.9.3 // indirect
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+type Foo struct {
+	ID        int
+	FirstName string `tag_name:"tag 1"`
+	LastName  string `tag_name:"tag 2"`
+	Age       int    `tag_name:"tag 3"`
+}
+
+func (f Foo) TableName() string {
+	return "foo"
+}
+
+func main() {
+	var foo Foo;
+}
